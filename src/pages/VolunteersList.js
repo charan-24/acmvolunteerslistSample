bbc3528fdc68a0c9de51af3b6930dab451d1ce1c
charan-24 <charanpasupunooti137@gmail.com>
2022-10-10 21:26:14 +0530

    commit-4

--- src/pages/VolunteersList.js
+++ src/pages/VolunteersList.js
@@ -1,140 +1,155 @@
 export const Candidates=[
     {
-        key:1,
         Name:"srinibha srikanth",
         domain:"Social media",
         emailid:"srinibha.srikanth@gmail.com",
     },
+
     {
-        key:2,
         Name:"Aashritha Bibireddy",
         domain:"Social Media",
         emailid:"aashrithabibireddy@gmail.com",
     },
+
     {
-        key:3,
         Name:"L.varunesh",
         domain:"Social media",
         emailid:"lvarunesh26@gmail.com",
     },
+
     {
-        key:4,
         Name:"S.Rishikesh",
         domain:"Social Media",
         emailid:"rishikeshsara03@gmail.com",
     },
+
     {
-        key:5,
         Name:"R.Sai Adithya",
         domain:"Social Media",
         emailid:"aditxyaa@gmail.com",
     },
+    
     {
-        key:6,
         Name:"Neela Varun Gopa",
         domain:"Social Media",
         emailid:"varunvr2203@gmail.com",
     },
+
     {
-        key:7,
         Name:"Kambhampati Sai Harsha Vardhan",
         domain:"Technical",
         emailid:"harshaspartax18@gmail.com",
     },
+
     {
-        key:8,
         Name:"Kodaru Jayanth",
         domain:"Technical",
         emailid:"jayanthroy555@gmail.com",
     },
+
     {
-        key:9,
         Name:"Suryateja Chittiprolu",
         domain:"Technical",
         emailid:"suryatejach04@gmail.com",
     },
+
     {
-        key:10,
+
         Name:"Imadabathuni Suhrutha",
         domain:"Technical",
         emailid:"suhrutha2004@gmail.com",
     },
+
     {
-        key:11,
+
         Name:"Shanmukhi",
         domain:"Technical",
         emailid:"shanmukhikurapati2425@gmail.com",
     },
+
     {
-        key:12,
+
         Name:"Chandrakethan Sivarsthri",
         domain:"Technical",
         emailid:"s.chandrakethan9@gmail.com",
     },
+
     {
-        key:13,
+
         Name:"N Sravya Reddy",
         domain:"Events and PR",
         emailid:"Sravyareddy.n06@gmail.com",
     },
+
     {
-        key:14,
+
         Name:"BIllakanti Krishna sai",
         domain:"Events and PR",
         emailid:"krishnasaibillakanti08@gmail.com",
     },
+
     {
-        key:15,
+
         Name:"Sri Swapnika Dodla",
         domain:"Events and PR",
         emailid:"sriswapnikad@gmail.com",
     },
+
     {
-        key:16,
+
         Name:"N.Siddharth Reddy",
         domain:"Events and PR",
         emailid:"siddharthreddy210603@gmail.com",
     },
+
     {
-        key:17,
+
         Name:"Sai Meghana Maradana",
         domain:"Social Media",
         emailid:"saimeghanait@gmail.com",
     },
+
     {
-        key:18,
+
         Name:"Chilla Srivaibhav",
         domain:"Design",
         emailid:"srivaibhavchilla2923@gmail.com",
     },
+
     {
-        key:19,
+
         Name:"J.Vinish Reddy",
         domain:"Design",
         emailid:"vinishreddy1604@gmail.com",
     },
+
     {
-        key:20,
+
         Name:"Lalitha Bhavana M",
         domain:"Design",
         emailid:"lalithabhavana9@gmail.com",
     },
+
     {
-        key:21,
+
         Name:"Praneetha Madala",
         domain:"Design",
         emailid:"praneetha.Madala@gmail.com",
     },
+
     {
-        key:22,
+
         Name:"Komma vishal",
         domain:"Design",
         emailid:"kommavishal@gmail.com",
     },
+
     {
-        key:23,
+
         Name:"Potluri Srinija",
         domain:"Design",
         emailid:"srinijapotluri@gmail.com",
     },
+    
+
 ]
